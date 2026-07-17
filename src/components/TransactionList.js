3fb3c6880b35792fe9ee117c8d54c27143b0d68b
Pranav Nair <nairpranavprakash@gmail.com>
2026-07-17 15:15:54 +0530

    Fix: logic error

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -8,7 +8,7 @@ export const TransactionList = () => {
 
   return (
     <>
-      <h3>History</h3>
+      <h3>History of transactions</h3>
       <ul className="list">
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
